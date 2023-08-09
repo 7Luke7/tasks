6b258f60b270a7001c372ed187ee90b0ff716eec
7Luke7 <101182100+7Luke7@users.noreply.github.com>
2023-08-10 02:37:41 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 require("dotenv").config()
 const app = express()
-const port = 5001
+const PORT = process.env.PORT || 5000;
 const cors = require("cors")
 
 app.use(cors(
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/tasks", require("./routes"))
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
-    app.listen(port, () => {
-        console.log(`listening on port ${port}`)
+    app.listen(PORT, () => {
+        console.log(`listening on port ${PORT}`)
     })
 }).catch(err => console.log(err))
